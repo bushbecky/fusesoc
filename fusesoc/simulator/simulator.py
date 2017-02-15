bushbecky/fusesoc

commit 304cdf4ad4682e6f0286794aab009bb8483e73e6

Handle missing simulator toplevel in core.py

fusesoc/simulator/simulator.py
@@ -19,10 +19,6 @@ class Simulator(EdaTool):
         super(Simulator, self).__init__(system, export)
 
         logger.debug( "depend -->  " +str (self.cores))
-        if 'toplevel' in self.system.simulator:
-            self.toplevel = self.system.simulator['toplevel']
-        else:
-            self.toplevel = 'orpsoc_tb'
 
         self._get_vpi_modules()
 

fusesoc/core.py
@@ -52,6 +52,8 @@ class Core:
 
         #FIXME : Make simulators part of the core object
         self.simulator        = config.get_section('simulator')
+        if not 'toplevel' in self.simulator:
+            self.simulator['toplevel'] = 'orpsoc_tb'
 
         for s in section.load_all(config, core_file):
             if type(s) == tuple:

tests/test_capi1.py
@@ -51,3 +51,18 @@ def test_ise():
     assert core.ise.top_module == 'orpsoc_top'
 
     assert core.ise.warnings == []
+
+def test_simulator():
+    #Explicit toplevel
+    filename = os.path.join(os.path.dirname(__file__),
+                            __name__,
+                            "c3demo.core")
+    core = Core(filename)
+    assert core.simulator['toplevel'] == 'testbench'
+
+    #Implicit toplevel
+    filename = os.path.join(os.path.dirname(__file__),
+                            __name__,
+                            "atlys.core")
+    core = Core(filename)
+    assert core.simulator['toplevel'] == 'orpsoc_tb'

commit 525a09c2bf52164fe6ff552acb6c0c6ca7eb415a

Fix simulation that broke in 304cdf4

fusesoc/simulator/simulator.py
@@ -40,6 +40,7 @@ class Simulator(EdaTool):
                 self.vpi_modules += [vpi_module]
 
     def configure(self, args, skip_params = False):
+        self.toplevel = self.system.simulator['toplevel']
         if not skip_params:
             self.parse_args(args, 'sim', ['plusarg', 'vlogdefine', 'vlogparam'])
         super(Simulator, self).configure(args)